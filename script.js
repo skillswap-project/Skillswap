20e051f7c34d29df95b5e5eebe578a0aff162e42
skillswap-project <skillswap.projekt@gmail.com>
2025-05-14 14:47:09 +0200

    URL_Supabase script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 // Supabase Konfiguration
-const SUPABASE_URL = 'https://dein-projekt.supabase.co'
-const SUPABASE_ANON_KEY = 'dein-anon-key'
+const SUPABASE_URL = https://hmqzpwvofjvrlvkjwvgf.supabase.co
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtcXpwd3ZvZmp2cmx2a2p3dmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjQ5MzksImV4cCI6MjA2MjgwMDkzOX0.RxhzSRQ4MC_McVrBvS2o2WyPyFegidWwnN5N6m8qXF8
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 const loginForm = document.getElementById('login-form')
